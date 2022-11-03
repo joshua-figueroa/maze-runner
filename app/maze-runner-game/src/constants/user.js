ae29eebacd5f5f27d57cd555d44304b9e143b17c
Joshua Figueroa <crescendo@Joshua-MacBook-Pro.local>
2022-11-04 04:54:10 +0800

    remove FE app

--- app/maze-runner-game/src/constants/user.js
+++
@@ -1,2 +0,0 @@
-export const USERNAME = "maze-runner-username";
-export const USERID = "maze-runner-userid";
--- app/maze-runner-game/src/util/socket.js
+++
@@ -1,1 +0,0 @@
-export const SOCKET_URL = process.env.NODE_ENV === "development" ? "http://localhost:8080/maze-runner-game" : "/maze-runner-game";
